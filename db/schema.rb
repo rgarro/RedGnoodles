0ce1d356c54233422f7f8e9a8d54b7bf73d240c2
Rolando Garro <rgarro@gmail.com>
2012-03-08 12:43:03 -0600

    adding jqgrid plugin and initial gingredient values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120213000141) do
+ActiveRecord::Schema.define(:version => 20120308182847) do
 
   create_table "dishes", :force => true do |t|
     t.string   "title",                     :null => false
--- public/javascripts/kitchen.js
+++ public/javascripts/kitchen.js
@@ -1,13 +1,16 @@
 
 $(document).ready(function(){
 	var move = false;
-	var x = 17;
-	var pacPadding = 25;
+	var totalSteps=17;
+	var step = 0;
+	var pacPadding = 0;
+	var pacPaddingValue = 25;
 	
 	$("#preloadImg").hide();
 	
 	$("#preloadImg").ajaxStart(function(){
 		$(this).show();
+		setOrigin();
 		move=true;
 		walk();
 	});
@@ -23,9 +26,9 @@ $(document).ready(function(){
 		$("#preloadImg").css('width','500px');
 		$("#preloadImg").css('background-position',(pacPadding + 15)+'px 0px');
 		if(move){
-			x = x - 1;
-			pacPadding = pacPadding + 25;
-			if(x==0){
+			step = step - 1;
+			pacPadding = pacPadding + pacPaddingValue;
+			if(step==0){
 				setOrigin();		
 			}
 			setTimeout(walk,200);
@@ -38,8 +41,8 @@ $(document).ready(function(){
 	}
 	
 	function setOrigin(){
-		x=17;
-		pacPadding = 25;
+		step=totalSteps;
+		pacPadding = pacPaddingValue;
 	}
 		
 });
